29aeba69b04c06b9ebf34d7be2241e67c05fe6fd
Ningyi Xue <Ginny@dhcp-wifi-8021x-155-41-94-22.bu.edu>
2017-06-06 12:15:15 -0400

    HW1

---
+++ hw1/routes/hw1.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Ginny on 17/6/5.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+// var request = require("request");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+
+router.get('/:name', function (req, res, next) {
+    let theName = req.params.name
+    res.send(JSON.stringify({"string": theName, "length": theName.length}))
+//    next()
+})
+
+// router.param('/', function (req, res, next, value) {
+//     req.params.name
+//     next()
+// })
+
+router.post('/', function (req, res, next) {
+    res.json(req.body)
+})
+
+// router.post('/', function (req, res, next) {
+//     console.log(req.body.hw1)
+//     for (item in req.body) {
+//         console.log(item)
+//     }
+//     res.json(req.body.hw1)
+// })
+module.exports = router;
